ab5ccba1bbabf0bd9830b4e6705fab4865071512
yzhu <yzhu@homeaway.com>
2016-12-20 15:09:20 +1100

    Configuration updates.

--- config/environment.dev.ts
+++ config/environment.dev.ts
@@ -1,5 +1,6 @@
 
 export const environment = {
   production: false,
+  // API_BASE: "http://52.64.202.186/cgi-bin"
   API_BASE: "http://app01.yesbookit.com/cgi-bin"
 };
--- config/environment.prod.ts
+++ config/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  API_BASE: "http://52.64.202.186/cgi-bin"
 };
